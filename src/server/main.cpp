f7794f3c19cb729a628dd143f0a6846fceec42cb
Andrew Chen <andrewtheannihilator@gmail.com>
2012-07-28 19:33:08 -0700

    publishing serialized gamestate

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -5,6 +5,8 @@
 #include "vector"
 #include "GameState.h"
 #include "iostream"
+#include "NetworkConstants.h"
+#include "Debug.h"
 
 using std::cout;
 using std::endl;
@@ -12,6 +14,30 @@ using std::endl;
 int main() {
 	cout << "TODO: write server" << endl;
 
+	// std::vector<Elf> elves;
+	// elves.push_back((Elf) {1, 0, 0});
+	// elves.push_back((Elf) {2, 0, 1});
+	// elves.push_back((Elf) {3, 1, 0});
+	// GameState state1;
+	// state1.elves = elves;
+	// state1.felhound = (Felhound) {2,2};
+
+	// std::string serialized = serializeGameState(state1);
+
+	// GameState state2;
+	// state2 = deserializeGameState(serialized);
+
+
+	// std::cout << gameStateToString(state1) << std::endl;
+	// std::cout << serialized << std::endl;
+	// std::cout << gameStateToString(state2) << std::endl;
+
+	IPaddress address;
+	SDLNet_ResolveHost(&address, "127.0.0.1", 2000);
+
+	ClientConnection connection(address);
+
+	// GameState state;
 	std::vector<Elf> elves;
 	elves.push_back((Elf) {1, 0, 0});
 	elves.push_back((Elf) {2, 0, 1});
@@ -20,29 +46,14 @@ int main() {
 	state1.elves = elves;
 	state1.felhound = (Felhound) {2,2};
 
-	std::string serialized = serializeGameState(state1);
+	bool running = true;
+	while(running){
+		connection.sendGameState(state1);
+		DEBUG("Sending state");
+		DEBUG(gameStateToString(state1));
+		SDL_Delay(1000);
+	}
 
-	GameState state2;
-	state2 = deserializeGameState(serialized);
-
-
-	std::cout << gameStateToString(state1) << std::endl;
-	std::cout << serialized << std::endl;
-	std::cout << gameStateToString(state2) << std::endl;
 	return 0;
 
-	// IPaddress address;
-	// SDLNet_ResolveHost(&address, "127.0.0.1", 2000);
-
-	// ClientConnection connection(address);
-
-	// GameState state;
-	// bool running = true;
-	// while(running){
-	// 	connection.sendGameState(state);
-	// 	SDL_Delay(1000);
-	// }
-
-
-	// ClientConnection connection()
 }
