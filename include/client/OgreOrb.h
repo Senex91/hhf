f74354b60dff218a378cf5d853d716401f7dc016
Andrew Chen <andrewtheannihilator@gmail.com>
2012-08-03 21:15:38 -0700

    added OgreOrb and OgreFelhound

---
+++ include/client/OgreOrb.h
@@ -0,0 +1,46 @@
+#ifndef OGREORB_H
+#define OGREORB_H
+
+#define OgreOrb_MESHNAME "plane727.mesh"
+#define OgreOrb_ENTITY_NAME "OgreOrb"
+#define OgreOrb_MATERIAL_NAME "planeMaterial"
+
+#include <OgreColourValue.h>
+#include <OgreSubEntity.h>
+#include <OgreSceneManager.h>
+#include <OgreSceneNode.h>
+#include <OgreEntity.h>
+#include <string>
+
+class OgreOrb {
+public:
+    OgreOrb(Ogre::SceneManager*, int);
+    ~OgreOrb(void);
+
+    void setPosition(float, float, float);
+    // void setOrientation(float);
+    void setColour(Ogre::ColourValue);
+
+    static Ogre::ColourValue getColour(unsigned int);
+
+
+private:
+
+    // static Ogre::ColourValue[] generateColors();
+
+    int id;
+    std::string name;
+    
+
+    Ogre::SceneManager* manager;
+    Ogre::SceneNode* node;
+    Ogre::Entity* entity;
+
+    // static Ogre::ColourValue colors[];
+
+
+
+
+};
+
+#endif
