bb82784b00efab73f6964bbdd1d93dec85a15d4e
Adriaan Cody Schuffelen <astronomix@gmail.com>
2012-08-20 05:29:29 -0700

    Fixed CommandTestSuite to use new GameState

--- include/shared/tests/CommandTestSuite.h
+++ include/shared/tests/CommandTestSuite.h
@@ -42,15 +42,17 @@ public:
 	}
 	void testGameStateCommand() {
 		std::vector<Elf> elves;
-		elves.push_back((Elf) {1, 0, 0, 0, 0});
-		elves.push_back((Elf) {2, 0, 1, 0, 0});
-		elves.push_back((Elf) {3, 1, 0, 0, 0});
+		elves.push_back((Elf) {1, 0, 0, 0, 0, 0.0f, 0.1, 0.2, (ElfColor){1.0f,0.0f,0.0f}, true});
+		elves.push_back((Elf) {2, 0, 1, 0, 0, 1.0f, 0.3, 0.4, (ElfColor){0.0f,1.0f,0.0f}, true});
+		elves.push_back((Elf) {3, 1, 0, 0, 0, 2.0f, 0.5, 0.6, (ElfColor){0.0f,0.0f,1.0f}, false});
 		GameState state1;
 		state1.elves = elves;
-		state1.felhound = (Felhound) {2,2, 0, 0};
+		state1.felhound = (Felhound) {2,2, 0, 0, false};
 		state1.orb.x = 0;
 		state1.orb.y = 0;
 		state1.orb.id = -1;
+		state1.orb.alive = 0;
+		
 		
 		GameStateCommand c(state1);
 		std::string out = c.write();
