8d546df70378481ee57ff3ee2c4e31c59b8cf060
Adriaan Cody Schuffelen <astronomix@gmail.com>
2012-09-03 14:57:58 -0500

    Fixed CommandTestSuite to use new state enums

--- include/shared/tests/CommandTestSuite.h
+++ include/shared/tests/CommandTestSuite.h
@@ -42,16 +42,16 @@ public:
 	}
 	void testGameStateCommand() {
 		std::map<int,Elf> elves;
-		elves[0] = ((Elf) {1, 0, 0, 0, 0, 0.0f, 0.1, 0.2, (ElfColor){1.0f,0.0f,0.0f}, true});
-		elves[1] = ((Elf) {2, 0, 1, 0, 0, 1.0f, 0.3, 0.4, (ElfColor){0.0f,1.0f,0.0f}, true});
-		elves[2] = ((Elf) {3, 1, 0, 0, 0, 2.0f, 0.5, 0.6, (ElfColor){0.0f,0.0f,1.0f}, false});
+		elves[0] = ((Elf) {1, 0, 0, 0, 0, 0.0f, 0.1, 0.2, (ElfColor){1.0f,0.0f,0.0f}, ELF_UNSPAWNED});
+		elves[1] = ((Elf) {2, 0, 1, 0, 0, 1.0f, 0.3, 0.4, (ElfColor){0.0f,1.0f,0.0f}, ELF_SPAWNED});
+		elves[2] = ((Elf) {3, 1, 0, 0, 0, 2.0f, 0.5, 0.6, (ElfColor){0.0f,0.0f,1.0f}, ELF_DEAD});
 		GameState state1;
 		state1.elves = elves;
-		state1.felhound = (Felhound) {2,2, 0, 0, false};
+		state1.felhound = (Felhound) {2, 2, 0, 0, 1.0, FELHOUND_RESTING};
 		state1.orb.x = 0;
 		state1.orb.y = 0;
 		state1.orb.id = -1;
-		state1.orb.alive = 0;
+		state1.orb.state = ORB_UNSPAWNED;
 		
 		
 		GameStateCommand c(state1);
